7ed47b20fc979351b71df1d5f586ef680c9cba66
cglosner <cglosner@gmail.com>
2023-03-28 23:58:17 -0400

    Moved the sudo apt install commands

---
+++ install_libraries.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# make sure all of the necessary libraries are installed
+sudo apt install -y git acpica-tools python3-distutils uuid-dev nasm python-is-python3 gcc-multilib make g++ lcov
